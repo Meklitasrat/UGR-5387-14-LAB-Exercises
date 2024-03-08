75c5c80f48bc39502a8cd1d1af058c93db3b2f02
MEklit_Asrat <meklitzasrat@gmai.com>
2024-03-08 17:47:51 +0300

    Lab-exercises

--- LAB-1-Variables-and-Data-Types.dart
+++ LAB-1-Variables-and-Data-Types.dart
@@ -1,12 +1,9 @@
-void main(){
+void main() {
+  String name = "Meklit";
+  int age = 21;
 
-String name = "Meklit";
-int age = 21;
+  String favColor = "Peach";
 
-String favColor = "Peach";
-
-print(" My name is $name and I am $age years old. My favourte color is $favColor");
-
-
+  print(
+      " My name is $name and I am $age years old. My favourte color is $favColor");
 }
-

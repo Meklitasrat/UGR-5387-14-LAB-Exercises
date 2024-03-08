19959e069cc86677ba5edd8d10c62512e8800be1
MEklit_Asrat <meklitzasrat@gmai.com>
2024-03-08 15:28:49 +0300

    Lab-exercises

---
+++ LAB-3-Control-Flow.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+void main(){
+    print("Enter Number: ");
+  String input = stdin.readLineSync()!;
+  int number1 = int.parse(input);
+
+  print('Enter Operation :');
+  String oper = stdin.readLineSync()!;
+
+   print("Enter Another number: ");
+   String input2 = stdin.readLineSync()!; 
+  int number2 = int.parse(input2);
+
+
+switch(oper){
+  case '+':
+     print(number1 + number2);
+  
+  case '-':
+    print(number1 - number2 );
+
+  case '*':
+     print(number1 * number2);
+
+  case '/':
+     print(number1 / number2 );
+
+}
+}
+
